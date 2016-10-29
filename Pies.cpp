9a2df2c254c794cff86873679b7c63c5100635d9
bkmiecik12 <bkmiecik12@gmail.com>
2016-10-29 17:46:36 +0200

    Initial commit

---
+++ Pies.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Pies.h"
+
+using namespace std;
+
+Pies::Pies()
+
+void Pies::dajGlos()
+{
+	cout<<"Hau! ";
+}
---
+++ Zwierze.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Pies
+{
+
+	public:
+
+	Pies();
+	~Pies();
+	void dajGlos();
+
+};
